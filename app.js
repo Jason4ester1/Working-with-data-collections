d23207335a515fcb7cff713729a148ee8825cf46
Forrester <jason.forrester@accenture.com>
2024-05-21 09:42:06 -0500

    lets_go_again

--- app.js
+++ app.js
@@ -18,13 +18,20 @@ function parseCSV(csvString) {
           //      ["98","Bill","Doctor’s" ,"Assistant","26"]]
           
      // PART - 3 
-        let csvData = [{ id: "42", name: "Bruce", occupation: "knight", age: "41"},
-                 { id: "57", name: "Bob", occupation: "Fry Cook", age: "19"},
-                 { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58"},
-                 { id: "98", name: "Bill", occupation: "Doctor's Assistant", age: "26" }]
+        //let csvData = [{ id: "42", name: "Bruce", occupation: "knight", age: "41"},
+            //     { id: "57", name: "Bob", occupation: "Fry Cook", age: "19"},
+              //   { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58"},
+                // { id: "98", name: "Bill", occupation: "Doctor's Assistant", age: "26" }]
  
  //    PART - 4    
-                csvData.pop();
+               // csvData.pop();
+               // csvData.push({id: "48", name: "Barry", occupation: "Runner",age: "25"})
+               // csvData.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" })
+          
+      // PART - 5
+          //let csvData = ID,Name,Occupation,Age;\r42,Bruce,Knight,41;\r57,Bob,Fry Cook,19\r63,Blaine,Quiz Master, 58;\r98,Bill,Doctor’s Assistant,26
 
- 
-          console.log(csvData);
+          
+          
+               console.log(csvData);
+

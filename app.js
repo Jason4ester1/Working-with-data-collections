e4c7c1ec65c1a0b15067add128431c3b29a5029c
Forrester <jason.forrester@accenture.com>
2024-05-22 13:36:36 -0500

    last-commit

--- app.js
+++ app.js
@@ -6,30 +6,30 @@ function parseCSV(csvString) {
       }
   }
       //PART - 1
-  //let csvData = ID,Name,Occupation,Age;\n42,Bruce,Knight,41;\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master, 58;\n98,Bill,Doctor’s Assistant,26
+ let csvData = ["ID","occupation","Age","\n42","Bruce","Knight","41","\n57","Bob","Fry Cook" ,"19","\n63","Blaine","Quiz Master"," 58","\n98","Bill","Doctor’s Assistant","26"]
 
 
 
-    //PART - 2 
-  // let csvData = [["ID","Name","Occupation","Age"],
-   //              ["42","Bruce","Knight","41"],
-      //          ["57","Bob","Fry Cook","19"],
-         //       ["63","Blaine","Quiz Master","58"],
-          //      ["98","Bill","Doctor’s" ,"Assistant","26"]]
+   // PART - 2 
+  let csvDataTwo = [["ID","Name","Occupation","Age"],
+                ["42","Bruce","Knight","41"],
+               ["57","Bob","Fry Cook","19"],
+               ["63","Blaine","Quiz Master","58"],
+               ["98","Bill","Doctor’s" ,"Assistant","26"]]
           
-     // PART - 3 
-        //let csvData = [{ id: "42", name: "Bruce", occupation: "knight", age: "41"},
-            //     { id: "57", name: "Bob", occupation: "Fry Cook", age: "19"},
-              //   { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58"},
-                // { id: "98", name: "Bill", occupation: "Doctor's Assistant", age: "26" }]
+    // PART - 3 
+        let csvDataThree = [{ id: "42", name: "Bruce", occupation: "knight", age: "41"},
+              { id: "57", name: "Bob", occupation: "Fry Cook", age: "19"},
+              { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58"},
+              { id: "98", name: "Bill", occupation: "Doctor's Assistant", age: "26" }]
  
- //    PART - 4    
-               // csvData.pop();
-               // csvData.push({id: "48", name: "Barry", occupation: "Runner",age: "25"})
-               // csvData.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" })
+    // PART - 4    
+               csvData.pop();
+               csvData.push({id: "48", name: "Barry", occupation: "Runner",age: "25"})
+               csvData.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" })
           
       // PART - 5
-          //let csvData = ID,Name,Occupation,Age;\r42,Bruce,Knight,41;\r57,Bob,Fry Cook,19\r63,Blaine,Quiz Master, 58;\r98,Bill,Doctor’s Assistant,26
+          let csvDataFour = "ID";"Occupation","Age";"\r42","Bruce","Knight","41" ; "\r57","Bob","Fry Cook" ," 19" ;"Blaine","Quiz Master" , "58";"\r98","Bill","Doctor’s Assistant","26";
 
           
           
